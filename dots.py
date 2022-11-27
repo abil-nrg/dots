7273b8256c68b5bc0a2fdd637e1405e747e5ba77
sam-peets <sampeets@uvic.ca>
2022-11-27 09:55:04 -0800

    asdf

--- dots.py
+++ dots.py
@@ -1,7 +1,15 @@
 def main():
     gridsize = 3
+    maxmoves = gridsize*(gridsize-1)*2
+    v = [0]*(gridsize*(gridsize-1))
+    h = [0]*(gridsize*(gridsize-1))
+    
+    while True:
 
 
+def playerMove():
+    m = input('move: ')
+    if m[0] == "v"`
     
 if __name__ == "__main__":
     main()
